ed7a5fd30872ff37bc8377a66b9c5304125d7865
harshadalohar123 <harshalohar123@gmail.com>
2022-04-10 11:43:54 +0530

    [Harshada]Added:Check Duplicate Name

---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace AddressBook
+{
+    public class Contacts
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string email { get; set; }
+        public int zip { get; set; }
+        public long phoneNumber { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Address book problem");
+
+            PersonDetails records = new PersonDetails();
+            while (true) // It will iterate infinite loop.
+            {
+                Console.WriteLine("\nEnter 1 to Add persons details");
+                Console.WriteLine("Enter 2 for Display");
+                int choice = int.Parse(Console.ReadLine()); // here i am converting into int
+
+                switch (choice)
+                {
+                    case 1:
+                        records.AddRecord();
+                        break;
+                    case 2:
+                        records.DetailsOfPerson();
+                        break;
+                    default:
+                        Console.WriteLine("Please select valid input");
+                        break;
+
+                }
+            }
+        }
+    }
+}
